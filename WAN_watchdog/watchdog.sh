86edc61f29c1cb29a86524c1020fbf22ddb988c4
uAltair <opalakua@gmail.com>
2021-01-09 19:25:40 +0200

    Update watchdog.sh

--- WAN_watchdog/watchdog.sh
+++ WAN_watchdog/watchdog.sh
@@ -8,6 +8,11 @@ do
         logger "WAN_WATCHDOG ping 8.8.8.8 OK"
         exit 0
     fi
+    if /bin/ping -c 1 1.1.1.1 >/dev/null
+    then
+        logger "WAN_WATCHDOG ping 1.1.1.1 OK"
+        exit 0
+    fi
     tries=$((tries+1))
 done
 logger "WAN_WATCHDOG restart network  !!!"
